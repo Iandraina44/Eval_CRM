7d0650fb5f2ff3399142d6aeb92fa3d8d7c46557
Iandraina44 <iandrainaarivonjy@gmail.com>
2025-03-23 17:17:00 +0300

    budget depense et allertes

--- java_crm/src/main/java/site/easy/to/build/crm/service/depense/DepenseService.java
+++ java_crm/src/main/java/site/easy/to/build/crm/service/depense/DepenseService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import site.easy.to.build.crm.entity.Depense;
+import site.easy.to.build.crm.entity.Lead;
 
 public interface DepenseService {
     List<Depense> getAllDepenses();
     Optional<Depense> getDepenseById(Integer id);
     Depense saveDepense(Depense depense);
     void deleteDepense(Integer id);
+    double getTotalDepenseByCustomerId(int customerId);
+  
 }
--- java_crm/src/main/java/site/easy/to/build/crm/service/depense/DepenseServiceImpl.java
+++ java_crm/src/main/java/site/easy/to/build/crm/service/depense/DepenseServiceImpl.java
@@ -1,5 +1,6 @@
 package site.easy.to.build.crm.service.depense;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,15 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import site.easy.to.build.crm.entity.Depense;
+import site.easy.to.build.crm.entity.Lead;
+import site.easy.to.build.crm.entity.Notification;
 import site.easy.to.build.crm.repository.DepenseRepository;
+import site.easy.to.build.crm.service.budget.BudgetService;
+import site.easy.to.build.crm.service.notification.NotificationService;
+import site.easy.to.build.crm.service.notification.NotificationServiceImpl;
 
 @Service
 public class DepenseServiceImpl implements DepenseService {
+    
     private final DepenseRepository depenseRepository;
-
+   
     @Autowired
-    public DepenseServiceImpl(DepenseRepository depenseRepository) {
+    public DepenseServiceImpl(DepenseRepository depenseRepository ){
         this.depenseRepository = depenseRepository;
+
     }
 
     @Override
@@ -37,4 +45,19 @@ public class DepenseServiceImpl implements DepenseService {
     public void deleteDepense(Integer id) {
         depenseRepository.deleteById(id);
     }
+
+    @Override
+    public double getTotalDepenseByCustomerId(int customerId) {
+        return depenseRepository.getTotalDepenseByCustomerId(customerId);
+    }
+    // public void createDepense(Lead lead, double newDepense){
+    //     Depense depense = new Depense();
+    //     depense.setLead(lead);
+    //     Notification notif = budgetService.checkBudget(lead.getCustomer().getCustomerId(), newDepense);
+    //     if (notif.getMessage()!=null) {
+    //         notificationService.save(notif);    
+    //     }
+    //     depenseRepository.save(depense);
+        
+    // }
 }

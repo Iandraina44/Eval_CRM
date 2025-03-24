fa33648d6cf01f52cf6259a7692f277af87f7e18
Iandraina44 <iandrainaarivonjy@gmail.com>
2025-03-24 14:10:06 +0300

    Update BudgetRestController.java

--- java_crm/src/main/java/site/easy/to/build/crm/controller/BudgetRestController.java
+++ java_crm/src/main/java/site/easy/to/build/crm/controller/BudgetRestController.java
@@ -1,39 +1,40 @@
 package site.easy.to.build.crm.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import site.easy.to.build.crm.entity.Budget;
 import site.easy.to.build.crm.entity.StatistiqueBudget;
 import site.easy.to.build.crm.service.budget.BudgetService;
 import site.easy.to.build.crm.service.statistique.StatistiqueBudgetService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*") // Permet les appels CORS depuis n'importe quel domaine
 public class BudgetRestController {
 
     private final BudgetService budgetService;
     private final StatistiqueBudgetService statistiqueBudgetService;
 
-    @Autowired
     public BudgetRestController(BudgetService budgetService, StatistiqueBudgetService statistiqueBudgetService) {
         this.budgetService = budgetService;
         this.statistiqueBudgetService = statistiqueBudgetService;
     }
 
     @GetMapping("/budgets")
-    public List<Budget> getAllBudgets() {
-        return budgetService.findAll();
+    public ResponseEntity<List<Budget>> getAllBudgets() {
+        List<Budget> budgets = budgetService.findAll();
+        return ResponseEntity.ok(budgets);
     }
+
 
     @GetMapping("/statistiques")
-    public List<StatistiqueBudget> getAllStatistiques() {
-        return statistiqueBudgetService.getAllStatistiques();
+    public ResponseEntity<List<StatistiqueBudget>> getAllStatistiques() {
+        List<StatistiqueBudget> statistiques = statistiqueBudgetService.getAllStatistiques();
+        return ResponseEntity.ok(statistiques);
     }
 
-
+   
 }
